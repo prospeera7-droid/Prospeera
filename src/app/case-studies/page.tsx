e31d045f7c754e939a4ec1922396f6c0ec6beaaf
Harshil Bhoraniya <harshilbhoraniya10@gmail.com>
2025-09-21 10:11:45 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid: e

--- src/app/case-studies/page.tsx
+++ src/app/case-studies/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from 'next/link';
