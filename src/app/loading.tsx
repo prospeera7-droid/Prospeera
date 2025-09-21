5006a450d5af9392dc7811c503423d97d831666f
Harshil Bhoraniya <harshilbhoraniya10@gmail.com>
2025-09-21 09:50:35 +0000

    i could not see loading page before website start

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,9 @@
-"use client"
-
-import RotatingEarth from "@/components/RotatingEarth"
+"use client";
 
 export default function Loading() {
   return (
-    <div className="min-h-screen bg-background w-full flex justify-center items-center">
-          <RotatingEarth width={700} height={500}/>
+    <div className="fixed inset-0 flex items-center justify-center bg-background z-[100]">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
     </div>
-  )
+  );
 }
