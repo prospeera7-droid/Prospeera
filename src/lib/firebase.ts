3723f742ee0101db55c261bd0f2182bf63cf2c3a
prospeera7 <prospeera7@gmail.com>
2025-10-19 14:29:52 +0000

    firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUHiVmCu4Hfo1_x0PA7-LBcB1nZ-3Jgas",
-  authDomain: "prospeera-853b2.firebaseapp.com",
-  projectId: "prospeera-853b2",
-  storageBucket: "prospeera-853b2.firebasestorage.app",
-  messagingSenderId: "541675951559",
-  appId: "1:541675951559:web:b12c82153fa0c0d53b75e2",
-  measurementId: "G-HM1NYWH6GT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
